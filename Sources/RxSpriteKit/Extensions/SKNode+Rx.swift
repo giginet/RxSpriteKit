c0feecde16955b4d3a67e07be1bab1aa60690974
giginet <giginet.net@gmail.com>
2016-04-16 18:08:46 +0900

    Add extensions for SKNode property

--- Sources/RxSpriteKit/Extensions/SKNode+Rx.swift
+++ Sources/RxSpriteKit/Extensions/SKNode+Rx.swift
@@ -6,12 +6,10 @@ extension SKNode {
     public var rx_position: ControlProperty<CGPoint> {
         let source = Observable<CGPoint>.create { observer in
             observer.on(.Next(self.position))
-            return AnonymousDisposable {
-                print("dispose")
+            return NopDisposable.instance
             }
-        }
-        .distinctUntilChanged()
-        .takeUntil(self.rx_deallocated)
+            .distinctUntilChanged()
+            .takeUntil(self.rx_deallocated)
         
         let bindingObserver = UIBindingObserver(UIElement: self) { node, position in
             node.position = position
@@ -19,10 +17,57 @@ extension SKNode {
         return ControlProperty(values: source, valueSink: bindingObserver)
     }
     
+    public var rx_scale: AnyObserver<CGFloat> {
+        return UIBindingObserver(UIElement: self) { node, scale in
+            node.setScale(scale)
+            }.asObserver()
+    }
     
+    public var rx_xScale: AnyObserver<CGFloat> {
+        return UIBindingObserver(UIElement: self) { node, xScale in
+            node.xScale = xScale
+            }.asObserver()
+    }
+    
+    public var rx_yScale: AnyObserver<CGFloat> {
+        return UIBindingObserver(UIElement: self) { node, yScale in
+            node.yScale = yScale
+            }.asObserver()
+    }
+    
+    public var rx_zRotation: AnyObserver<CGFloat> {
+        return UIBindingObserver(UIElement: self) { node, zRotation in
+            node.zRotation = zRotation
+            }.asObserver()
+    }
+    
+    public var rx_alpha: AnyObserver<CGFloat> {
+        return UIBindingObserver(UIElement: self) { node, alpha in
+            node.alpha = alpha
+            }.asObserver()
+    }
+
     public var rx_hidden: AnyObserver<Bool> {
-        return UIBindingObserver(UIElement: self) { view, hidden in
-            view.hidden = hidden
+        return UIBindingObserver(UIElement: self) { node, hidden in
+            node.hidden = hidden
+            }.asObserver()
+    }
+    
+    public var rx_name: AnyObserver<String?> {
+        return UIBindingObserver(UIElement: self) { node, name in
+            node.name = name
+            }.asObserver()
+    }
+    
+    public var rx_speed: AnyObserver<CGFloat> {
+        return UIBindingObserver(UIElement: self) { node, speed in
+            node.speed = speed
+            }.asObserver()
+    }
+    
+    public var rx_paused: AnyObserver<Bool> {
+        return UIBindingObserver(UIElement: self) { node, paused in
+            node.paused = paused
             }.asObserver()
     }
 }
--- Tests/SKNode+RxTest.swift
+++ Tests/SKNode+RxTest.swift
@@ -24,6 +24,64 @@ class SKNodeRxTests: QuickSpec {
                 expect(node.position).to(equal(CGPointMake(10, 10)))
             }
             
+            it("rx_scale") {
+                let variable = Variable<CGFloat>(1.0)
+                variable.asObservable().bindTo(node.rx_scale).addDisposableTo(disposeBag)
+                
+                expect(node.xScale).to(equal(1.0))
+                expect(node.yScale).to(equal(1.0))
+                
+                variable.value = 0.5
+                
+                expect(node.xScale).to(equal(0.5))
+                expect(node.yScale).to(equal(0.5))
+            }
+            
+            it("rx_xScale") {
+                let variable = Variable<CGFloat>(1.0)
+                variable.asObservable().bindTo(node.rx_xScale).addDisposableTo(disposeBag)
+                
+                expect(node.xScale).to(equal(1.0))
+                
+                variable.value = 0.5
+                
+                expect(node.xScale).to(equal(0.5))
+            }
+            
+            
+            it("rx_yScale") {
+                let variable = Variable<CGFloat>(1.0)
+                variable.asObservable().bindTo(node.rx_yScale).addDisposableTo(disposeBag)
+                
+                expect(node.yScale).to(equal(1.0))
+                
+                variable.value = 0.5
+                
+                expect(node.yScale).to(equal(0.5))
+            }
+            
+            it("rx_zRotation") {
+                let variable = Variable<CGFloat>(1.0)
+                variable.asObservable().bindTo(node.rx_zRotation).addDisposableTo(disposeBag)
+                
+                expect(node.zRotation).to(equal(1.0))
+                
+                variable.value = 0.5
+                
+                expect(node.zRotation).to(equal(0.5))
+            }
+            
+            it("rx_alpha") {
+                let variable = Variable<CGFloat>(1.0)
+                variable.asObservable().bindTo(node.rx_alpha).addDisposableTo(disposeBag)
+                
+                expect(node.alpha).to(equal(1.0))
+                
+                variable.value = 0.5
+                
+                expect(node.alpha).to(equal(0.5))
+            }
+            
             it("rx_hidden") {
                 let hidden = Variable<Bool>(false)
                 hidden.asObservable().bindTo(node.rx_hidden).addDisposableTo(disposeBag)
@@ -33,6 +91,38 @@ class SKNodeRxTests: QuickSpec {
                 hidden.value = true
                 expect(node.hidden).to(beTrue())
             }
+            
+            it("rx_name") {
+                let variable = Variable<String?>(nil)
+                variable.asObservable().bindTo(node.rx_name).addDisposableTo(disposeBag)
+                
+                expect(node.name).to(beNil())
+                
+                variable.value = "newName"
+                
+                expect(node.name).to(equal("newName"))
+            }
+            
+            it("rx_speed") {
+                let variable = Variable<CGFloat>(1.0)
+                variable.asObservable().bindTo(node.rx_speed).addDisposableTo(disposeBag)
+                
+                expect(node.speed).to(equal(1.0))
+                
+                variable.value = 0.5
+                
+                expect(node.speed).to(equal(0.5))
+            }
+            
+            it("rx_paused") {
+                let variable = Variable<Bool>(false)
+                variable.asObservable().bindTo(node.rx_paused).addDisposableTo(disposeBag)
+                
+                expect(node.paused).to(beFalse())
+                
+                variable.value = true
+                expect(node.paused).to(beTrue())
+            }
         }
-    }    
+    }
 }
