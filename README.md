a45f1565ac8fcb81c392be6e0d1198acbca0bbd0
giginet <giginet.net@gmail.com>
2016-04-16 17:46:55 +0900

    Add SKNode+Rx test cases

---
+++ README.md
@@ -0,0 +1,6 @@
+RxSpriteKit
+=======================
+
+[![Build Status](https://travis-ci.org/giginet/RxSpriteKit.svg?branch=master)](https://travis-ci.org/giginet/RxSpriteKit)
+
+Under development
--- Tests/SKNode+RxTest.swift
+++ Tests/SKNode+RxTest.swift
@@ -6,10 +6,33 @@ import SpriteKit
 
 class SKNodeRxTests: QuickSpec {
     override func spec() {
-        describe("SKNode extension") {            
+        describe("SKNode extension") {
+            let disposeBag: DisposeBag = DisposeBag()
+            var node: SKNode!
+            
+            beforeEach {
+                node = SKNode()
+            }
+            
             it("rx_position") {
-                expect(1 + 1).to(equal(2))
+                let point = Variable<CGPoint>(CGPointZero)
+                point.asObservable().bindTo(node.rx_position).addDisposableTo(disposeBag)
+                
+                expect(node.position).to(equal(CGPointZero))
+                
+                point.value = CGPointMake(10, 10)
+                expect(node.position).to(equal(CGPointMake(10, 10)))
+            }
+            
+            it("rx_hidden") {
+                let hidden = Variable<Bool>(false)
+                hidden.asObservable().bindTo(node.rx_hidden).addDisposableTo(disposeBag)
+                
+                expect(node.hidden).to(beFalse())
+                
+                hidden.value = true
+                expect(node.hidden).to(beTrue())
             }
         }
-    }
+    }    
 }
