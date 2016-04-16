faf159970bff0286a991ccb6e7ce3cf28b5ddf2e
giginet <giginet.net@gmail.com>
2016-04-16 14:41:45 +0900

    Intiial Commit

---
+++ Sources/RxSpriteKit/RxSpriteKit.h
@@ -0,0 +1,19 @@
+//
+//  RxSpriteKit.h
+//  RxSpriteKit
+//
+//  Created by giginet on 4/16/16.
+//  Copyright © 2016 giginet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxSpriteKit.
+FOUNDATION_EXPORT double RxSpriteKitVersionNumber;
+
+//! Project version string for RxSpriteKit.
+FOUNDATION_EXPORT const unsigned char RxSpriteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSpriteKit/PublicHeader.h>
+
+
