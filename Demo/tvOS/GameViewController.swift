7a1ddec42882b88f326d823cb989ed0bdfe154af
giginet <giginet.net@gmail.com>
2018-02-05 23:05:44 +0900

    Add demo app for tvOS

---
+++ Demo/tvOS/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  RxSpriteKitDemo
+//
+//  Created by giginet on 2018/02/05.
+//  Copyright © 2018 giginet. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
