15649f836046d8979226c6934d21491246138e91
giginet <giginet.net@gmail.com>
2018-02-09 23:24:11 +0900

    Separate host app of watch extension

---
+++ Demo/watchOS/Host/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSpriteKitDemoHost
+//
+//  Created by giginet on 2018/02/09.
+//  Copyright © 2018 giginet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
