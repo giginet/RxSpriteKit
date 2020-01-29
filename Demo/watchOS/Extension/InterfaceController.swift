c1e76d732fb16ca0269a567ca0412463d2ca0f30
giginet <giginet.net@gmail.com>
2020-01-30 01:04:17 +0900

    Apply swiftlint

--- Demo/watchOS/Extension/InterfaceController.swift
+++ Demo/watchOS/Extension/InterfaceController.swift
@@ -9,35 +9,34 @@
 import WatchKit
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet var skInterface: WKInterfaceSKScene!
-    
+
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+
         // Configure interface objects here.
-        
+
         // Load the SKScene from 'GameScene.sks'
         if let scene = GameScene(fileNamed: "GameScene") {
-            
+
             // Set the scale mode to scale to fit the window
             scene.scaleMode = .aspectFill
-            
+
             // Present the scene
             self.skInterface.presentScene(scene)
-            
+
             // Use a value that will maintain a consistent frame rate
             self.skInterface.preferredFramesPerSecond = 30
         }
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
