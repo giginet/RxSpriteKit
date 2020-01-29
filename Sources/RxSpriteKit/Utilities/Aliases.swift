2484391f93be794d401fe185eb6e7a1bb6f23b0e
giginet <giginet.net@gmail.com>
2020-01-30 00:50:26 +0900

    Fix tests

--- Sources/RxSpriteKit/Utilities/Aliases.swift
+++ Sources/RxSpriteKit/Utilities/Aliases.swift
@@ -2,10 +2,9 @@ import Foundation
 
 #if os(iOS) || os(tvOS) || os(watchOS)
     import UIKit
-    public typealias RSKLineBreakMode = NSLineBreakMode
     public typealias RSKColor = UIColor
 #else
     import Cocoa
-    public typealias RSKLineBreakMode = NSParagraphStyle.LineBreakMode
     public typealias RSKColor = NSColor
 #endif
+public typealias RSKLineBreakMode = NSLineBreakMode
